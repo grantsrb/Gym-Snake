e738e876fad7082cc1cacdef2b13439c78571177
Satchel Grant <grantsrb@gmail.com>
2018-02-01 08:19:51 -0800

    remove link to github

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(name='gym_snake',
       version='0.0.1',
-      url=https://github.com/grantsrb/Gym-Snake,
       author="Satchel Grant",
       install_requires=['gym', 'numpy', 'matplotlib'],
       python_requires='>=3'
