8d1dcb1f5558f6cb6c7fdb700e03c0ec7a798ab5
Saetch <grantsrb@gmail.com>
2023-12-16 11:33:02 -0600

    minor refactor to update plural

--- gym_snake/envs/snake_extrahard_env.py
+++ gym_snake/envs/snake_extrahard_env.py
@@ -3,47 +3,22 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 from gym_snake.envs.snake import Controller, Discrete
+from gym_snake.envs.snake_env import SnakeEnv
 
 try:
     import matplotlib.pyplot as plt
 except ImportError as e:
     raise error.DependencyNotInstalled("{}. (HINT: see matplotlib documentation for installation https://matplotlib.org/faq/installing_faq.html#installation".format(e))
 
-class SnakeExtraHardEnv(gym.Env):
+class SnakeExtraHardEnv(SnakeEnv):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, grid_size=[25,25], unit_size=10, unit_gap=1, snake_size=5, n_snakes=3, n_foods=2, random_init=True):
-        self.grid_size = grid_size
-        self.unit_size = unit_size
-        self.unit_gap = unit_gap
-        self.snake_size = snake_size
-        self.n_snakes = n_snakes
-        self.n_foods = n_foods
-        self.viewer = None
-        self.action_space = Discrete(4)
-        self.last_obs = None
-        self.random_init = random_init
-
-    def step(self, action):
-        self.last_obs, rewards, done, info = self.controller.step(action)
-        return self.last_obs, rewards, done, info
-
-    def reset(self):
-        self.controller = Controller(self.grid_size, self.unit_size, self.unit_gap, self.snake_size, self.n_snakes, self.n_foods, random_init=self.random_init)
-        self.last_obs = self.controller.grid.grid
-        return self.last_obs
-
-    def render(self, mode='human', close=False, frame_speed=.1):
-        if self.viewer is None:
-            self.fig = plt.figure()
-            self.viewer = self.fig.add_subplot(111)
-            plt.ion()
-            self.fig.show()
-        else:
-            self.viewer.clear()
-            self.viewer.imshow(self.last_obs)
-            plt.pause(frame_speed)
-        self.fig.canvas.draw()
-
-    def seed(self, x):
-        pass
+        super().__init__(
+            grid_size=grid_size,
+            unit_size=unit_size,
+            unit_gap=unit_gap,
+            snake_size=snake_size,
+            n_snakes=n_snakes,
+            n_foods=n_foods,
+            random_init=random_init)
